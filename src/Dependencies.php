42cb1ad95755645850e1dfbe637198d1bfe7a471
Jomino Domino <jomino.domino@gmail.com>
2019-07-26 19:38:57 +0200

    reorder code

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -14,42 +14,38 @@ class Dependencies
             return new \Slim\Csrf\Guard;
         };
 
-        // View
-        $twig = new \Slim\Views\Twig($container->settings['view']['path'], $container->settings['view']['twig']);
-        $twig->addExtension(new \Slim\Views\TwigExtension($container->router, $container->request->getUri()));
-        $twig->addExtension(new \Util\TranslatorExtension($container->trans));
-        $twig->addExtension(new \Util\CsrfExtension($container->csrf));
-        $twig->addExtension(new \Twig_Extension_Debug());
+        // Translation
+        $container['trans'] = function ($container) use($app) {
+            $loader = new \Illuminate\Translation\FileLoader(new \Illuminate\Filesystem\Filesystem(), $container->settings['localisation']['path']);
+            $translator = new \Illuminate\Translation\Translator($loader, $app->language);
+            return $translator;
+        };
 
-        $container['view'] = function ($container) use($twig) {
+        // View
+        $container['view'] = function ($container) {
+            $twig = new \Slim\Views\Twig($container->settings['view']['path'], $container->settings['view']['twig']);
+            $twig->addExtension(new \Slim\Views\TwigExtension($container->router, $container->request->getUri()));
+            $twig->addExtension(new \Util\TranslatorExtension($container->trans));
+            $twig->addExtension(new \Util\CsrfExtension($container->csrf));
+            $twig->addExtension(new \Twig_Extension_Debug());
             return $twig;
         };
 
         // Logger
-        $logger = new \Monolog\Logger($container->settings['logger']['name']);
-        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-        $logger->pushHandler(new \Monolog\Handler\StreamHandler($container->settings['logger']['path'], $container->settings['logger']['level']));
-
-        $container['logger'] = function ($container) use($logger) {
+        $container['logger'] = function ($container) {
+            $logger = new \Monolog\Logger($container->settings['logger']['name']);
+            $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+            $logger->pushHandler(new \Monolog\Handler\StreamHandler($container->settings['logger']['path'], $container->settings['logger']['level']));
             return $logger;
         };
 
         // DB
-        $capsule = new \Illuminate\Database\Capsule\Manager;
-        $capsule->addConnection($container->settings['db']['env']);
-        $capsule->setAsGlobal();
-        $capsule->bootEloquent();
-
-        $container['db'] = function ($container) use($capsule) {
+        $container['db'] = function ($container) {
+            $capsule = new \Illuminate\Database\Capsule\Manager;
+            $capsule->addConnection($container->settings['db']['env']);
+            $capsule->setAsGlobal();
+            $capsule->bootEloquent();
             return $capsule->getConnection()->query();
-        };
-
-        // Translation
-        $loader = new \Illuminate\Translation\FileLoader(new \Illuminate\Filesystem\Filesystem(), $container->settings['localisation']['path']);
-
-        $container['trans'] = function ($container) use($app,$loader) {
-            $translator = new \Illuminate\Translation\Translator($loader, $app->language);
-            return $translator;
         };
 
     }
