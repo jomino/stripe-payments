d16147fe8f188eb46ea7e17d0b7ef56dbfea9488
Jomino Domino <jomino.domino@gmail.com>
2019-07-27 18:27:23 +0200

    add sql file alt for phinx proc

---
+++ sql/stripe_users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `stripe_users` (
+  `id` int(9) unsigned NOT NULL auto_increment,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `active` tinyint unsigned NOT NULL,
+  `uuid` varchar(256) NOT NULL default '',
+  `name` varchar(128) NOT NULL default '',
+  `email` varchar(128) NOT NULL default '',
+  `pkey` varchar(256) NOT NULL default '',
+  `skey` varchar(256) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
