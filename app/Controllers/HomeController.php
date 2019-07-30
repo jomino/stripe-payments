f824e59f42fd06dfa6525746ba9fad50b4a3e46a
Jomino Domino <jomino.domino@gmail.com>
2019-07-31 00:59:05 +0200

    improve security

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,6 +6,13 @@ class HomeController extends \Core\Controller
 {
     public function __invoke($request, $response, $args)
     {
+        $cookie = \Util\Tools::cookieGetValue(\Dflydev\FigCookies\FigRequestCookies::get($request, \App\Parameters::SECURITY['cookie'], 'none'));
+        $pass_phrase = \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret'];
+        if($cookie!=hash('sha256', $pass_phrase)){
+            if($cookie!='none'){ $response = \Dflydev\FigCookies\FigResponseCookies::remove($response, \App\Parameters::SECURITY['cookie']); }
+        }else{
+            return $response->withRedirect($this->router->pathFor('adduser'), 301);
+        }
         return $this->view->render($response, 'Home/index.html.twig');
     }
 }
--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -7,29 +7,20 @@ class LoginController extends \Core\Controller
     public function __invoke($request, $response, $args)
     {
         $parsedBody = $request->getParsedBody();
-        $cookie = \Util\Tools::cookieGetValue(\Dflydev\FigCookies\FigRequestCookies::get($request, \App\Parameters::SECURITY['cookie'], 'none'));
-        $pass_phrase = \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret'];
-        $hash = hash('sha256', $pass_phrase);
-        if($cookie=='none'){
-            if($parsedBody['login']==\App\Parameters::SECURITY['login']){
-                $response = \Dflydev\FigCookies\FigResponseCookies::set($response, \Dflydev\FigCookies\SetCookie::create(\App\Parameters::SECURITY['cookie'])
-                    ->withPath('/')
-                    ->withValue($hash)
-                    ->withMaxAge(30*60)
-                    ->withDomain($request->getUri()->getHost())
-                    ->withSecure(true)
-                    ->withHttpOnly(true)
-                );
-            }else{
-                $response = \Dflydev\FigCookies\FigResponseCookies::remove($response, \App\Parameters::SECURITY['cookie']);
-                return $response->withStatus(403);
-            }
+        if($parsedBody['login']==\App\Parameters::SECURITY['login']){
+            $pass_phrase = \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret'];
+            $hash = hash('sha256', $pass_phrase);
+            $response = \Dflydev\FigCookies\FigResponseCookies::set($response, \Dflydev\FigCookies\SetCookie::create(\App\Parameters::SECURITY['cookie'])
+                ->withPath('/')
+                ->withValue($hash)
+                ->withMaxAge(30*60)
+                ->withDomain($request->getUri()->getHost())
+                ->withSecure(true)
+                ->withHttpOnly(true)
+            );
+            return $response->withRedirect($this->router->pathFor('adduser'), 301);
         }else{
-            if($cookie!=$hash){
-                $response = \Dflydev\FigCookies\FigResponseCookies::remove($response, \App\Parameters::SECURITY['cookie']);
-                return $response->withStatus(403);
-            }
+            return $response->withRedirect($this->router->pathFor('home'), 301);
         }
-        return $response->withRedirect($this->router->pathFor('adduser'), 301);
     }
 }
