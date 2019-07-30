824ee7f5fdfb13a1bf2b5fc7146d009c21c536a6
Jomino Domino <jomino.domino@gmail.com>
2019-07-31 01:00:53 +0200

    refactoring

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -9,10 +9,9 @@ class LoginController extends \Core\Controller
         $parsedBody = $request->getParsedBody();
         if($parsedBody['login']==\App\Parameters::SECURITY['login']){
             $pass_phrase = \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret'];
-            $hash = hash('sha256', $pass_phrase);
             $response = \Dflydev\FigCookies\FigResponseCookies::set($response, \Dflydev\FigCookies\SetCookie::create(\App\Parameters::SECURITY['cookie'])
                 ->withPath('/')
-                ->withValue($hash)
+                ->withValue(hash('sha256', $pass_phrase))
                 ->withMaxAge(30*60)
                 ->withDomain($request->getUri()->getHost())
                 ->withSecure(true)
