0398decb9ee1c858b932119d611441b6815c5830
Jomino Domino <jomino.domino@gmail.com>
2019-07-30 14:25:02 +0200

    add fig-cookies

--- app/Controllers/AddUserController.php
+++ app/Controllers/AddUserController.php
@@ -6,6 +6,12 @@ class AddUserController extends \Core\Controller
 {
     public function __invoke($request, $response, $args)
     {
+        $secret = \App\Parameters::SECURITY['secret'];
+        $parsedBody = $request->getParsedBody();
+        $skey = $parsedBody['skey'];
+        if($secret!=$skey){
+            return $response->withStatus(403);
+        }
         return $this->view->render($response, 'Home/adduser.html.twig');
     }
 }
