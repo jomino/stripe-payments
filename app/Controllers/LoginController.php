5a6977df7b8faa073cc0dc705ae19c0e2a8616d0
Jomino Domino <jomino.domino@gmail.com>
2019-07-30 22:03:03 +0200

    corrections

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -2,24 +2,19 @@
 
 namespace App\Controllers;
 
-use App\Parameters as Parameters;
-use Util\Tools as Tools;
-
-use Carbon\Carbon as Carbon;
-
 class LoginController extends \Core\Controller  
 {
     public function __invoke($request, $response, $args)
     {
         $parsedBody = $request->getParsedBody();
-        $cookie = Tools::cookieGetValue(\Dflydev\FigCookies\FigRequestCookies::get($request, Parameters::SECURITY['cookie'], 'none'));
-        $pass_phrase = Parameters::SECURITY['login'].'-'.Parameters::SECURITY['secret'];
+        $cookie = \Util\Tools::cookieGetValue(\Dflydev\FigCookies\FigRequestCookies::get($request, \App\Parameters::SECURITY['cookie'], 'none'));
+        $pass_phrase = \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret'];
         $hash = hash('sha256', $pass_phrase);
         if($cookie=='none'){
-            if($parsedBody['login']==Parameters::SECURITY['login']){
-                $dtc = Carbon::now(new \DateTimeZone('Europe/Brussels'));
+            if($parsedBody['login']==\App\Parameters::SECURITY['login']){
+                $dtc = \Carbon\Carbon::now(new \DateTimeZone('Europe/Brussels'));
                 $dtc->addMinutes(30);
-                $response = \Dflydev\FigCookies\FigResponseCookies::set($response, \Dflydev\FigCookies\SetCookie::create(Parameters::SECURITY['cookie'])
+                $response = \Dflydev\FigCookies\FigResponseCookies::set($response, \Dflydev\FigCookies\SetCookie::create(\App\Parameters::SECURITY['cookie'])
                     ->withPath('/')
                     ->withValue($hash)
                     ->withExpires($dtc->toCookieString())
--- app/Middleware/LoginMiddleware.php
+++ app/Middleware/LoginMiddleware.php
@@ -2,17 +2,13 @@
 
 namespace App\Middleware;
 
-use App\Parameters;
-
-use Dflydev\FigCookies\FigRequestCookies;
-
 class LoginMiddleware
 {
 
     public function __invoke($request, $response, $next){
-        $hash = hash('sha256', Parameters::SECURITY['login'].'-'.Parameters::SECURITY['secret']);
-        $cookie = Tools::cookieGetValue(FigRequestCookies::get($request, Parameters::SECURITY['cookie'], 'none'));
-        $request = $request->withAttribute(Parameters::SECURITY['status'], $cookie==$hash);
+        $hash = hash('sha256', \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret']);
+        $cookie = \Util\Tools::cookieGetValue(\Dflydev\FigCookies\FigRequestCookies::get($request, \App\Parameters::SECURITY['cookie'], 'none'));
+        $request = $request->withAttribute(\App\Parameters::SECURITY['status'], $cookie==$hash);
         return $next($request, $response);
     }
 
