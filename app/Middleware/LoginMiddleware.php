802e937b4d25253e4aee66340139138683ef7077
Jomino Domino <jomino.domino@gmail.com>
2019-07-30 21:12:37 +0200

    correction security cookie

--- app/Middleware/LoginMiddleware.php
+++ app/Middleware/LoginMiddleware.php
@@ -11,7 +11,7 @@ class LoginMiddleware
 
     public function __invoke($request, $response, $next){
         $hash = hash('sha256', Parameters::SECURITY['login'].'-'.Parameters::SECURITY['secret']);
-        $cookie = FigRequestCookies::get($request, Parameters::SECURITY['cookie'], 'none');
+        $cookie = Tools::cookieGetValue(FigRequestCookies::get($request, Parameters::SECURITY['cookie'], 'none'));
         $request = $request->withAttribute(Parameters::SECURITY['status'], $cookie==$hash);
         return $next($request, $response);
     }
