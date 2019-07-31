78d60436003eea15a21d49796e6a5b3d2a86b96c
Jomino Domino <jomino.domino@gmail.com>
2019-07-31 15:44:18 +0200

    add stripe utility

---
+++ util/StripeUtility.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Util;
+
+class StripeUtility
+{
+
+    private static $WEBHOOK_ENABLE_EVENTS = ["charge.failed", "charge.succeeded"];
+
+    public static function createWebhook($api_key,$wh_url)
+    {
+        \Stripe\Stripe::setApiKey($api_key);
+
+        $raw = \Stripe\WebhookEndpoint::create([
+          "url" => $wh_url,
+          "enabled_events" => self::$WEBHOOK_ENABLE_EVENTS
+        ]);
+
+        return json_decode($raw);
+
+    }
+
+}
