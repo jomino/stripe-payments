a2a0f269cbc2d98444f78a7a527b4885302600cd
Jomino Domino <jomino.domino@gmail.com>
2019-07-31 19:20:26 +0200

    correction

--- src/Middleware.php
+++ src/Middleware.php
@@ -13,7 +13,7 @@ class Middleware
     public function __construct($app)
     {
         $app->add(new \Util\AcceptLanguage($app));
-        $app->add(new \app\Middleware\StripeMiddleware($app));
+        $app->add(new \App\Middleware\StripeMiddleware($app));
     }
 
 }
