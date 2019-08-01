c63f1a0a1f2dc3dbec135b0538253b68e7bfac04
Jomino Domino <jomino.domino@gmail.com>
2019-08-01 11:57:14 +0200

    add stripe_signature if any to midd

--- app/Middleware/StripeMiddleware.php
+++ app/Middleware/StripeMiddleware.php
@@ -12,6 +12,8 @@ class StripeMiddleware
     public function __invoke($request, $response, $next){
         $logger = $this->app->getContainer()->get('logger');
         \Stripe\Stripe::setLogger($logger);
+        $stripe_signature = $request->getHeaderLine('stripe-signature');
+        $request = $request->withAttribute('stripe_signature',$stripe_signature);
         return $next($request, $response);
     }
 
