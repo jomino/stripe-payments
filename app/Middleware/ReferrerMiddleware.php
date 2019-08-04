dd0a35e3e0b87f038a843b6207a631ab55ba182a
Jomino Domino <jomino.domino@gmail.com>
2019-08-04 22:34:10 +0200

    add session referrer

--- app/Middleware/ReferrerMiddleware.php
+++ app/Middleware/ReferrerMiddleware.php
@@ -6,11 +6,17 @@ class ReferrerMiddleware
 {
 
     public function __invoke($request, $response, $next){
-        $referrer = $request->getHeaderLine('Referer');
-        if(empty($referrer)){
-            $referrer = $request->getServerParam('HTTP_REFERER', '');
+        $session = $this->get('session');
+        if(!$session->exists('referrer')){
+            $referrer = $request->getHeaderLine('Referer');
+            if(empty($referrer)){
+                $referrer = $request->getServerParam('HTTP_REFERER', '');
+            }
+            $referrer = preg_replace('#^(?:http[s]?://)?([a-z0-9\-._~%]+)(?:/?.*)$#i','$1',$referrer);
+            $session->set('referrer',$referrer);
+        }else{
+            $referrer = $session->get('referrer');
         }
-        $referrer = preg_replace('#^(?:http[s]?://)?([a-z0-9\-._~%]+)(?:/?.*)$#i','$1',$referrer);
         $request = $request->withAttribute('referrer',$referrer);
         return $next($request, $response);
     }
