45ce2e270b5ff1f3f167f4186d92a51a3b88938b
Jomino Domino <jomino.domino@gmail.com>
2019-08-04 17:59:19 +0200

    debug referrer

--- app/Controllers/StripePaymentController.php
+++ app/Controllers/StripePaymentController.php
@@ -6,6 +6,8 @@ class StripePaymentController extends \Core\Controller
 {
     public function choice($request, $response, $args)
     {
-        return $this->view->render($response, 'Home/payselect.html.twig');
+        //return $this->view->render($response, 'Home/payselect.html.twig');
+            $content = $request->getAttribute('referrer');  
+            return $response->write('Referer: '.$content);
     }
 }
