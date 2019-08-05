215f310bd0496d9127d6da8f173c11ca1820f01b
Jomino Domino <jomino.domino@gmail.com>
2019-08-05 18:09:30 +0200

    correction named routes

--- src/Routes.php
+++ src/Routes.php
@@ -28,10 +28,10 @@ class Routes
 
         // Payments
         $app->group( '', function($app){
-            $app->get('/{token:[0-9a-zA-Z-]*}/{amount:[0-9]*}', \App\Controllers\StripePaymentController::class.':start')->setName('payment-start');
-            $app->post('/identify', \App\Controllers\StripePaymentController::class.':identify')->setName('payment-identify');
-            $app->post('/source', \App\Controllers\StripePaymentController::class.':source')->setName('payment-source');
-            $app->get('/result/{token:[0-9a-zA-Z-]*}', \App\Controllers\StripePaymentController::class.':result')->setName('payment-result');
+            $app->get('/{token:[0-9a-zA-Z-]*}/{amount:[0-9]*}', \App\Controllers\StripePaymentController::class.':start')->setName('payment_start');
+            $app->post('/identify', \App\Controllers\StripePaymentController::class.':identify')->setName('payment_identify');
+            $app->post('/source', \App\Controllers\StripePaymentController::class.':source')->setName('payment_source');
+            $app->get('/result/{token:[0-9a-zA-Z-]*}', \App\Controllers\StripePaymentController::class.':result')->setName('payment_result');
         })->add($container->get('csrf'));
         // Infos/Debug
         $app->get('/infos', function($request, $response, $args){
