954fb7251e4844202e534fe4d87e95cd55c001c9
Jomino Domino <jomino.domino@gmail.com>
2019-08-05 14:13:27 +0200

    refactoring

--- assets/js/application.js
+++ assets/js/application.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
     }
 
     var pkey = window.STRIPE_PUBLISHABLE_KEY;
-    var amount = window.STRIPE_AMOUNT_VALUE;
 
     var carouselStepTo = function(step){
         $('.carousel').carousel(step);
@@ -18,7 +17,7 @@ $(document).ready(function(){
         carouselStepTo(1);
     });
 
-    $('a.step-btn').on('click',function(){
+    $('a.step-btn').on( 'click', function(){
         e.preventDefault();
         var step = parseInt($(this).data('step'));
         if(step<2){ carouselStepTo(step); }
