c144fabd479aaa091558d78f71ff34047d5fc739
Jomino Domino <jomino.domino@gmail.com>
2019-08-10 09:51:55 +0200

    refactoring

--- util/AcceptLanguage.php
+++ util/AcceptLanguage.php
@@ -10,8 +10,12 @@ class AcceptLanguage
     }
 
     public function __invoke($request, $response, $next){
-        $languageHeader = $request->getHeader('Accept-Language');
-        $this->app->language = substr($languageHeader[0],0,2);
+        $language = '';
+        if ($request->hasHeader('Accept-Language')) {
+            $accept_language = $request->getHeader('Accept-Language');
+            $language = substr($accept_language[0],0,2);
+        }
+        $this->app->language = $language;
         return $next($request, $response);
     }
 
