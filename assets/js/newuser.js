264f5a6aad770a925c8c97b51f00288a9757b113
Jomino Domino <jomino.domino@gmail.com>
2019-08-13 11:06:32 +0200

    correction jq

--- assets/js/newuser.js
+++ assets/js/newuser.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
     $('.generated-link .clickable').on('click',function(e){
         e.preventDefault();
-        var $input = $(this).closest('input');
+        var _this = this;
+        var $input = $('input[type="text"]',$(this).parent());
         $input[0].select();
         document.execCommand('copy');
-        $(this).tooltip('show',{
+        $(this).tooltip({
+            trigger: 'manual',
+            container: 'body',
             title: 'copié !!'
         });
+        $(this).tooltip('show');
+        window.setTimeout(function(){
+            $(_this).tooltip('hide');
+        },1500);
         return false;
     })
 });
