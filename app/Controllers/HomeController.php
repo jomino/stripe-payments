691a3224ce68af88cad663277230b3d3e5275939
Jomino Domino <jomino.domino@gmail.com>
2019-08-15 00:43:37 +0200

    add info log

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,9 +6,11 @@ class HomeController extends \Core\Controller
 {
     public function __invoke($request, $response, $args)
     {
+        $ip = $request->getServerParam('REMOTE_ADDR');
         $cookie = \Util\Tools::cookieGetValue(\Dflydev\FigCookies\FigRequestCookies::get($request, \App\Parameters::SECURITY['cookie'], 'none'));
         $pass_phrase = \App\Parameters::SECURITY['login'].'-'.\App\Parameters::SECURITY['secret'];
         if($cookie!=hash('sha256', $pass_phrase)){
+            $this->logger->info('['.$ip.'] ADMIN_LOGIN_OPENED');
             if($cookie!='none'){ $response = \Dflydev\FigCookies\FigResponseCookies::remove($response, \App\Parameters::SECURITY['cookie']); }
             return $this->view->render($response, 'Home/login.html.twig');
         }else{
