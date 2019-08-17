9c1178a81eb5762365fef8de7d952e09627bb168
Jomino Domino <jomino.domino@gmail.com>
2019-08-17 19:43:49 +0200

    add loader visual

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-    $('a.step-btn').on('click',function(e){
-        e.preventDefault();
-        return false;
-    })
+    
+    var $main_el = $('.container.main');
+    var $loading_el = $('.container.loader');
+
+    var overlayLoader = function(show,options){
+        if($.LoadingOverlay){
+            $loading_el.LoadingOverlay(show,options || {});
+        }
+    };
+
+    if($loading_el.length){
+        window.setTimeout(function(){
+            overlayLoader('hide');
+            $loading_el.remove();
+            $main_el.show();
+        },3000);
+        overlayLoader('show');
+    }else{
+        $main_el.show();
+    }
+
 });
