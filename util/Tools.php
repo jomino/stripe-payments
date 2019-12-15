0cd91087def75111ee84be58fc03db3bdfc0d42c
Jomino Domino <jomino.domino@gmail.com>
2019-12-15 12:28:41 +0100

    corrections

--- util/Tools.php
+++ util/Tools.php
@@ -14,11 +14,10 @@ class Tools
     public static function queryGetValues($qs)
     {
         $values = [];
-        $key_value = explode('&',$qs);
-        for ($i=0; $i < sizeof($key_value); $i++) {
-            $value =  explode('=',$key_value);
-            $values[$value[0]] = urldecode($value[1]);
+        if(preg_match('#^.*[%][0-9][a-z]#i',$qs)){
+            $qs = urldecode($qs);
         }
+        parse_str($qs, $values);
         return $values;
     }
 
